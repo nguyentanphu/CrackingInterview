568b4deabcafbd16a42959e668071207fedd2959
Phu.Nguyen <Phu.Nguyen@altsrc.net>
2020-03-18 18:11:37 +0700

    Update code

--- AlgoExpertEasy/Program.cs
+++ AlgoExpertEasy/Program.cs
@@ -7,19 +7,22 @@ namespace AlgoExpertEasy
 	{
 		static void Main(string[] args)
 		{
-			var arr = new int[] {1, 3, 5, 8, 2, 13, 7};
-			var sum = 100;
+			//var arr = new int[] {1, 3, 5, 8, 2, 13, 7};
+			//var sum = 100;
 
-			var result = FindSum(arr, sum);
+			//var result = FindSum(arr, sum);
 
-			if (result.HasValue)
-			{
-				Console.WriteLine($"{result.Value.Item1}, {result.Value.Item2}");
-			}
-			else
-			{
-				Console.WriteLine("Not found");
-			}
+			//if (result.HasValue)
+			//{
+			//	Console.WriteLine($"{result.Value.Item1}, {result.Value.Item2}");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Not found");
+			//}
+
+			var arr = new int[] { 1, 3, 5, 8, 2, 13, 7 };
+			var result = Find3Largest(arr);
 		}
 
 		static (int, int)? FindSum(int[] arr, int sum)
@@ -37,6 +40,27 @@ namespace AlgoExpertEasy
 			}
 
 			return null;
+		}
+
+		static int[] Find3Largest(int[] arr)
+		{
+			var result = new int[3];
+			foreach (var num in arr)
+			{
+				for (int j = 0; j < result.Length; j++)
+				{
+					if (result[j] == default)
+					{
+						result[j] = num;
+						break;
+					} else if (j < 2 && arr[j + 1] == default)
+					{
+						continue;
+					}
+				}
+			}
+
+			return result;
 		}
 	}
 }
